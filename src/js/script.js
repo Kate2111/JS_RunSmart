b48b62923dac5aa3abc36c8bd010427fb1b8344a
Kate <noskova2111@mail.ru>
2022-03-27 16:37:17 +0300

    validate form

--- src/js/script.js
+++ src/js/script.js
@@ -57,14 +57,41 @@ $(document).ready(function(){
 
     $('.modal__close').on('click', function() {
         $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
-    })
+    });
 
     $('.button_mini').each(function(i) {
         $(this).on('click', function() {
                 $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
                 $('.overlay, #order').fadeIn('slow');
         })
-    })
+    });
+
+    function valideForms(form) {
+        $(form).validate({
+            rules: {
+                name: 'required',
+                phone: 'required',
+                email: {
+                        required: true,
+                        email: true
+                }
+            },
+            messages: {
+                name: 'Пожалуйста, введите свое имя',
+                phone: 'Пожалуйста, введите свой номер телефона',
+                email: {
+                    required: 'Пожалуйста, введите свою почту',
+                    email: 'Неправильно введен адрес почты'
+                }
+            }
+        });
+    };
+
+    valideForms('#consultation-form');
+    valideForms('#consultation form');
+    valideForms('#order form');
+    
+    $('input[name=phone]').mask('+7 (999) 999-99-99');
 }); 
 
 
