9a5ae5d5502d862abf4bb26fa6ad8b783b912672
Kate <noskova2111@mail.ru>
2022-03-30 06:36:26 +0300

    add mailer php

--- src/js/script.js
+++ src/js/script.js
@@ -92,6 +92,22 @@ $(document).ready(function(){
     valideForms('#order form');
     
     $('input[name=phone]').mask('+7 (999) 999-99-99');
+
+    $('form').submit(function(e) { 
+        e.preventDefault();
+        $.ajax({
+            type: 'POST',
+            url: 'mailer/smart.php',
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find('input').val('');
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
 }); 
 
 
