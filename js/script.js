f7153bb29638aef4c4078e9e97331b9b38920c0c
Kate2111 <noskova2111@mail.ru>
2023-07-15 20:27:19 +0400

    Updates

--- js/script.js
+++ js/script.js
@@ -13,15 +13,18 @@ Undefined - существует, но не имеет никакого знач
 Symbol -  */
 
 $(document).ready(function(){
+    //slider
     $('.carousel__inner').slick({
-        speed: 1200,
+        speed: 500,
+        infinite: true,
+        variableWidth: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="img/left.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="img/right.png"></button>',
         responsive: [
                 {
-                        breakpoint: 992,
+                        breakpoint: 300,
                         settings: {
-                                speed: 1200,
+                                speed: 500,
                                 dots: true,
                                 arrows: false
                         }
@@ -29,6 +32,7 @@ $(document).ready(function(){
         ]
     });
 
+    //tabs
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
         $(this)
           .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
@@ -126,6 +130,133 @@ $(document).ready(function(){
     });
 
     new WOW().init();
-}); 
+
+      // Timer
+
+    const deadline = '2023-09-01';
+
+    function getTimeRemaning(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date());
+        const days = Math.floor(t / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((t / (1000 * 60 * 60) % 24));
+        const minutes = Math.floor((t / 1000 / 60) % 24);
+        const seconds = Math.floor(t / 1000 % 60);
+    
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds,
+        };
+    }
+    
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+    
+    function setClock(selector, endtime) {
+        const timer = $(selector);
+        const days = timer.find('#days');
+        const hours = timer.find('#hours');
+        const minutes = timer.find('#minutes');
+        const seconds = timer.find('#seconds');
+        const timeInterval = setInterval(updateClock, 1000);
+    
+        updateClock();
+    
+        function updateClock() {
+            const t = getTimeRemaning(endtime);
+    
+            days.text(getZero(t.days));
+            hours.text(getZero(t.hours));
+            minutes.text(getZero(t.minutes));
+            seconds.text(getZero(t.seconds));
+    
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+    
+    setClock('.timer', deadline);
+
+    class MenuCard {
+        constructor (src, alt, subtitle, descr, oldprice, price, parentSelector) {
+            this.src = src;
+            this.alt = alt;
+            this.subtitle = subtitle;
+            this.descr = descr;
+            this.oldprice = oldprice;
+            this.price = price;
+            this.parent = document.querySelector(parentSelector);
+        }
+            
+        render () {
+            const element = document.createElement('div');
+            element.innerHTML = `
+                <div class="catalog-item">
+                    <div class="catalog-item__wrapper">
+                        <div class="catalog-item__content catalog-item__content_active">
+                            <img src=${this.src}" alt=${this.alt} class="catalog-item__img">
+                            <div class="catalog-item__subtitle">${this.subtitle}</div>
+                            <div class="catalog-item__descr">${this.descr}</div>
+                            <a href="#" class="catalog-item__link">ПОДРОБНЕЕ</a>
+                        </div>
+                        <div class="catalog-item__list">
+                            <ul>
+                                <li>Вы услышите звуковое оповещение о нужном пульсе во время тренировки;</li>
+                                <li>Вы увидите информативный графический индикатор целевых тренировочных зон пульса;</li>
+                                <li>Также Вы увидите информацию о расходе калорий за тренировку;</li>
+                                <li>Вы сможете посмотреть данные по 10 тренировкам.</li>
+                            </ul>
+                            <a href="#" class="catalog-item__back">назад</a>
+                        </div>
+                    </div>
+                    <hr>
+
+                    <div class="catalog-item__footer">
+                        <div class="catalog-item__prices">
+                            <div class="catalog-item__old-price">${this.oldprice} руб.</div>
+                            <div class="catalog-item__price">${this.price} руб.</div>
+                        </div>
+                        <button class="button button_mini">КУПИТЬ</button>
+                    </div>
+                </div>
+            `;
+
+            this.parent.append(element);
+        }
+
+    }
+
+    new MenuCard(
+        "img/catalogue_model.jpg",
+        "catalog",
+        "Пульсометр Polar FT1",
+        "Для первых шагов в тренировках, основанных на сердечном ритме",
+        4750,
+        4500,
+        ".catalog .catalog__content_two",
+    ).render();
+
+    new MenuCard(
+        "img/bg_shoes.jpg",
+        "catalog",
+        "Пульсометр Polar FT1",
+        "Для первых шагов в тренировках, основанных на сердечном ритме",
+        4750,
+        4500,
+        ".catalog .catalog__content_two",
+    ).render();
+
+    
 
 
+});
+
+  
